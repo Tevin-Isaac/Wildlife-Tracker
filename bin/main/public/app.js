2ce71b1269b9855a4652ab4b413d27a30ec252e0
Tevin-creator <isaactevin67@gmail..com>
2021-08-18 11:05:07 +0300

    changed navbar and added container in layout.vtl

--- bin/main/public/app.js
+++ bin/main/public/app.js
@@ -1,50 +1,14 @@
-$(document).ready(function(){
-    // Animations init
-    new WOW().init();
-    
-    var typed=new Typed(`#type`,{
-         strings:[" Wildlife Tracker "," An application that allows Rangers to track wildlife sightings in the area. "],
-         backSpeed:70,
-         typeSpeed:80,
-         smartBackspace:true,
-         loop: true,
-       })
-       var typed =new Typed(`#msg`,{
-            strings:[" Your Animal has been saved ! "],
-            backSpeed:70,
-            typeSpeed:80,
-            smartBackspace:true,
-          })
-    })
-    
-    
-    function secondClick(){
-    timer:4000
-    Swal.fire(
-      'View Animals?',
-    )
-    }
-    
-    function firstClick(){
-    timer:4000
-    Swal.fire(
-      'Add endangered?',
-    
-    )
-    }
-    function thirdClick(){
-    timer:4000
-    Swal.fire(
-      'Add Sighting?',
-    
-    )
-    }
-    
-    
-    
-    
-    
-    
-    
-    // Animations init
-    new WOW().init();
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach((panel) => {
+  panel.addEventListener('click', () => {
+    removeActiveClasses();
+    panel.classList.add('active');
+  })
+})
+
+function removeActiveClasses() {
+  panels.forEach(panel => {
+    panel.classList.remove('active');
+  })
+}
